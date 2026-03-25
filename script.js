360b9230652b10b2160e5121fd418c288e9e7bb3
annisamarch <cinta9acin@gmail.com>
2026-03-25 21:26:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const submitButton = document.querySelector(".submit");
+    const popupContainer = document.querySelector(".popup-container");
+    const closeButton = document.querySelector(".close-btn");
+
+    // Pop-up initially hidden
+    popupContainer.style.display = "none";
+
+    submitButton.addEventListener("click", function() {
+        popupContainer.style.display = "flex";
+        submitButton.style.display = "none";
+    });
+
+    closeButton.addEventListener("click", function() {
+        popupContainer.style.display = "none";
+        submitButton.style.display = "block"; // balik ke kondisi awal
+    });
+});
